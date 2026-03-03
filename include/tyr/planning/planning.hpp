981273792f4a3f966fb7f075a04ff6be77664114
Dominik Drexler <drexlerd93@gmail.com>
2026-03-03 21:57:14 +0100

    add heuristic bindings

--- include/tyr/planning/planning.hpp
+++ include/tyr/planning/planning.hpp
@@ -35,6 +35,7 @@
 #include "tyr/planning/ground_task/state_repository.hpp"
 #include "tyr/planning/ground_task/successor_generator.hpp"
 #include "tyr/planning/ground_task/unpacked_state.hpp"
+#include "tyr/planning/heuristics/blind.hpp"
 #include "tyr/planning/lifted_task.hpp"
 #include "tyr/planning/lifted_task/axiom_evaluator.hpp"
 #include "tyr/planning/lifted_task/heuristics/add.hpp"
