a469d5a2c74257fcfbf94d20051abd7761bcd5dc
Dominik Drexler <drexlerd93@gmail.com>
2026-03-13 19:36:38 +0100

    added work on integrating compact relation tables

---
+++ include/tyr/formalism/datalog/ground_atom_2_data.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2025 Dominik Drexler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TYR_FORMALISM_DATALOG_GROUND_ATOM_2_DATA_HPP_
+#define TYR_FORMALISM_DATALOG_GROUND_ATOM_2_DATA_HPP_
+
+#include "tyr/common/types.hpp"
+#include "tyr/common/types_utils.hpp"
+#include "tyr/formalism/datalog/binding_view.hpp"
+#include "tyr/formalism/datalog/declarations.hpp"
+#include "tyr/formalism/datalog/ground_atom_index.hpp"
+#include "tyr/formalism/object_index.hpp"
+#include "tyr/formalism/predicate_index.hpp"
+
+namespace tyr
+{
+template<formalism::FactKind T>
+struct Data<formalism::datalog::GroundAtom<T>>
+{
+    Index<formalism::datalog::GroundAtom<T>> index;
+    IndexList<formalism::Object> objects;
+
+    Data() = default;
+    Data(Index<formalism::datalog::GroundAtom<T>> index, IndexList<formalism::Object> objects) : index(index), objects(objects) {}
+    Data(const Data& other) = delete;
+    Data& operator=(const Data& other) = delete;
+    Data(Data&& other) = default;
+    Data& operator=(Data&& other) = default;
+
+    void clear() noexcept
+    {
+        tyr::clear(index);
+        tyr::clear(objects);
+    }
+
+    auto cista_members() const noexcept { return std::tie(index, objects); }
+    auto identifying_members() const noexcept { return std::tie(index.group, objects); }
+};
+
+}
+
+#endif
---
+++ include/tyr/formalism/datalog/ground_atom_2_view.hpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2025 Dominik Drexler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TYR_FORMALISM_DATALOG_GROUND_ATOM_VIEW_HPP_
+#define TYR_FORMALISM_DATALOG_GROUND_ATOM_VIEW_HPP_
+
+#include "tyr/common/types.hpp"
+#include "tyr/common/vector.hpp"
+#include "tyr/formalism/datalog/declarations.hpp"
+#include "tyr/formalism/datalog/ground_atom_index.hpp"
+#include "tyr/formalism/object_view.hpp"
+#include "tyr/formalism/predicate_view.hpp"
+
+namespace tyr
+{
+template<formalism::FactKind T, formalism::datalog::Context C>
+class View<Index<formalism::datalog::GroundAtom<T>>, C>
+{
+private:
+    const C* m_context;
+    Index<formalism::datalog::GroundAtom<T>> m_handle;
+
+public:
+    View(Index<formalism::datalog::GroundAtom<T>> handle, const C& context) noexcept : m_context(&context), m_handle(handle) {}
+
+    const auto& get_data() const noexcept { return get_repository(*m_context)[m_handle]; }
+    const auto& get_context() const noexcept { return *m_context; }
+    const auto& get_handle() const noexcept { return m_handle; }
+
+    auto get_index() const noexcept { return m_handle; }
+    auto get_predicate() const noexcept { return make_view(m_handle.group, *m_context); }
+    auto get_objects() const noexcept { return make_view(get_data().objects, *m_context); }
+
+    auto identifying_members() const noexcept { return std::tie(m_context, m_handle); }
+};
+
+namespace formalism::datalog
+{
+template<formalism::FactKind T>
+using GroundAtomView = View<Index<GroundAtom<T>>, Repository>;
+
+template<formalism::FactKind T>
+using GroundAtomListView = View<IndexList<GroundAtom<T>>, Repository>;
+}
+}
+
+#endif
